bdb17e6f9941d7b0fe072fe49817b40a7d249980
shanayabarretto <barretto.shanaya@gmail.com>
2024-09-01 01:53:32 -0400

    website update

--- app.py
+++ app.py
@@ -6,18 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/hobbies')
-def hobbies():
-    return render_template("hobbies.html")
-
 @app.route('/work-experience')
 def jobs():
     return render_template("jobs.html")
 
-@app.route('/extracurriculars')
+@app.route('/design-teams')
 def clubs():
     return render_template("clubs.html")
 
 @app.route('/personal-projects')
 def projects():
     return render_template("projects.html")
+
+@app.route('/about-me')
+def about():
+    return render_template("about.html")
+
+@app.route('/resume')
+def resume():
+    return render_template("resume.html")
